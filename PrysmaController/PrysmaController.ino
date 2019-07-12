7a1b5236133595ed6a18cbd93b1fca8028cd8056
Nick Rook <Nick_Rook@intuit.com>
2019-07-11 20:54:42 -0700

    added prysmaID

--- PrysmaController/PrysmaController.ino
+++ PrysmaController/PrysmaController.ino
@@ -11,14 +11,25 @@
 #include "PrysmaWifi.h";
 #include "PrysmaOTA.h";
 
+char PRYSMA_ID[19];
+byte mac[6];
+
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("Booting");
+
+  // Generate this light's unique ID from the mac address
+  WiFi.macAddress(mac);
+  snprintf(PRYSMA_ID, sizeof(PRYSMA_ID), "Prysma-%02X%02X%02X%02X%02X%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  Serial.printf("[INFO]: %s Booting\n", PRYSMA_ID);
+
+  // Connect to WiFi
   setupWifi();
+
+  // Configure Over the air uploads
   setupOTA();
 
-  Serial.println("Ready");
+  Serial.printf("[INFO]: %s Ready\n", PRYSMA_ID);
   
 }
 
